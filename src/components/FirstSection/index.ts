d662f6ab866728acb157683e8b24fbfaa5dfff83
Mikabona96 <76862391+Mikabona96@users.noreply.github.com>
2022-10-26 14:19:06 +0300

    firstSection rtl

--- src/components/FirstSection/index.ts
+++ src/components/FirstSection/index.ts
@@ -14,38 +14,57 @@ export const firstSectionFunction = () => {
         const figure = (wrapper?.querySelector('.slide figure')) as HTMLElement;
         const width = figure.offsetWidth;
         let index = 0;
+        const rtl = !!firstSection?.classList.contains('rtl');
+
         const arrowNavigation = (operator: string) => {
             if (operator === '+') {
                 index += 1;
+                btns && btns?.length - 1 <= index ? index = btns?.length - 1 : index;
                 btns && index >= btns?.length - 1 ? index = btns?.length - 1 : index;
-                wrapper?.setAttribute('style', `transform: translateX(-${index * width}px)`);
+                wrapper?.setAttribute('style', `transform: translateX(${rtl ? '' : '-'}${index * width}px)`);
                 btns?.forEach((btn, i) => {
                     index === i ?  btn.classList.add('active') : btn.classList.remove('active');
                 });
+                console.log(index);
             } else {
                 index -= 1;
                 index < 0 ? index = 0 : index;
-                wrapper?.setAttribute('style', `transform: translateX(-${index * width}px)`);
+                wrapper?.setAttribute('style', `transform: translateX(${rtl ? '' : '-'}${index * width}px)`);
                 btns?.forEach((btn, i) => {
                     index === i ?  btn.classList.add('active') : btn.classList.remove('active');
                 });
+                console.log('here');
             }
-            index > 0 ? leftArrow?.setAttribute('style', 'display: block') : leftArrow?.setAttribute('style', 'display: none');
-            btns && index >= btns?.length - 1 ? rightArrow?.setAttribute('style', 'display: none') : rightArrow?.setAttribute('style', 'display: block');
+            if (firstSection?.classList.contains('rtl')) {
+                index > 0 ? rightArrow?.setAttribute('style', 'display: block') : rightArrow?.setAttribute('style', 'display: none');
+                btns && index >= btns?.length - 1 ? leftArrow?.setAttribute('style', 'display: none') : leftArrow?.setAttribute('style', 'display: block');
+            } else {
+                index > 0 ? leftArrow?.setAttribute('style', 'display: block') : leftArrow?.setAttribute('style', 'display: none');
+                btns && index >= btns?.length - 1 ? rightArrow?.setAttribute('style', 'display: none') : rightArrow?.setAttribute('style', 'display: block');
+            }
+            console.log(index);
         };
 
         const slideImages = (idx: number) => {
             index = idx;
+            console.log(index, idx);
             if (width > 576) {
                 index > 0 ? leftArrow?.setAttribute('style', 'display: block') : leftArrow?.setAttribute('style', 'display: none');
             }
-            btns && btns?.length - 1 === index ? rightArrow?.setAttribute('style', 'display: none') : rightArrow?.setAttribute('style', 'display: block');
-            console.log(index);
+            if (rtl) {
+                btns && btns?.length - 1 === index ? leftArrow?.setAttribute('style', 'display: none') : leftArrow?.setAttribute('style', 'display: block');
+                btns && btns?.length - 1 === index ? rightArrow?.setAttribute('style', 'display: block') : rightArrow?.setAttribute('style', 'display: none');
+                index === 0 ? rightArrow?.setAttribute('style', 'display: none') : rightArrow?.setAttribute('style', 'display: block');
+            } else {
+                btns && btns?.length - 1 === index ? rightArrow?.setAttribute('style', 'display: none') : rightArrow?.setAttribute('style', 'display: block');
+            }
 
             btns?.forEach((btn, i) => {
                 idx === i ?  btn.classList.add('active') : btn.classList.remove('active');
             });
-            idx === 0 ? wrapper?.setAttribute('style', 'transform: translateX(0px)') : wrapper?.setAttribute('style', `transform: translateX(-${idx * width}px)`);
+
+            idx === 0 ? wrapper?.setAttribute('style', 'transform: translateX(0px)') : wrapper?.setAttribute('style', `transform: translateX(${rtl ? '' : '-'}${idx * width}px)`);
+            console.log(index);
         };
 
         btns?.forEach((btn, idx) => {
@@ -53,12 +72,22 @@ export const firstSectionFunction = () => {
                 slideImages(idx);
             });
         });
-        rightArrow?.addEventListener('click', () => {
-            arrowNavigation('+');
-        });
-        leftArrow?.addEventListener('click', () => {
-            arrowNavigation('-');
-        });
+
+        if (rtl) {
+            rightArrow?.addEventListener('click', () => {
+                arrowNavigation('-');
+            });
+            leftArrow?.addEventListener('click', () => {
+                arrowNavigation('+');
+            });
+        } else  {
+            rightArrow?.addEventListener('click', () => {
+                arrowNavigation('+');
+            });
+            leftArrow?.addEventListener('click', () => {
+                arrowNavigation('-');
+            });
+        }
     });
 };
 firstSectionFunction();
