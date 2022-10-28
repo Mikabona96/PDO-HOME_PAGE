d111d9876a11ed5a7f684be11a05067d4c29a4bf
Mikabona96 <76862391+Mikabona96@users.noreply.github.com>
2022-10-28 12:59:45 +0300

    2nd section video background control play/pause

--- src/components/FourthSection/index.ts
+++ src/components/FourthSection/index.ts
@@ -11,55 +11,61 @@ export const fourthSectionFunction = () => {
         const width = section4?.clientWidth;
 
 
-        if (width && width <= 992) {
-            const removeActive = () => {
-                columns?.forEach((column) => {
-                    column.classList.remove('active');
-                });
-            };
+        const accordionHandler = () => {
+            if (width && width <= 992) {
+                const removeActive = () => {
+                    columns?.forEach((column) => {
+                        column.classList.remove('active');
+                    });
+                };
 
-            const accordionHandlerY = (idx: number) => {
-                columns && columns[ idx ].classList.contains('active') ? removeActive() : columns?.forEach((column, i) => {
-                    idx === i ? column.classList.add('active') : column.classList.remove('active');
-                });
-            };
+                const accordionHandlerY = (idx: number) => {
+                    columns && columns[ idx ].classList.contains('active') ? removeActive() : columns?.forEach((column, i) => {
+                        idx === i ? column.classList.add('active') : column.classList.remove('active');
+                    });
+                };
 
-            arrows?.forEach((arrow, i) => {
-                arrow.addEventListener('click', () => {
-                    accordionHandlerY(i);
+                arrows?.forEach((arrow, i) => {
+                    arrow.addEventListener('click', () => {
+                        accordionHandlerY(i);
+                    });
                 });
-            });
-        } else {
-            const removeActive = () => {
-                columns?.forEach((column) => {
-                    column.classList.remove('active');
-                    column.classList.remove('horizontal');
-                    column.setAttribute('style', 'width: 380px');
-                });
-            };
-            const accrdionHandlerX = (idx: number) => {
-                columns?.forEach((column, i) => {
-                    column.classList.remove('active');
-                    column.classList.add('horizontal');
-                    column.setAttribute('style', 'width: 180px');
-                    if (i === idx) {
+            } else {
+                const removeActive = () => {
+                    columns?.forEach((column) => {
+                        column.classList.remove('active');
                         column.classList.remove('horizontal');
-                        column.classList.add('active');
-                        column.setAttribute('style', 'width: 880px');
-                    }
-                });
-            };
+                        column.setAttribute('style', 'width: 380px');
+                    });
+                };
+                const accrdionHandlerX = (idx: number) => {
+                    columns?.forEach((column, i) => {
+                        column.classList.remove('active');
+                        column.classList.add('horizontal');
+                        column.setAttribute('style', 'width: 180px');
+                        if (i === idx) {
+                            column.classList.remove('horizontal');
+                            column.classList.add('active');
+                            column.setAttribute('style', 'width: 880px');
+                        }
+                    });
+                };
 
-            columns?.forEach((column, idx) => {
-                column.addEventListener('click', () => {
-                    if (column.classList.contains('active')) {
-                        removeActive();
-                    } else {
-                        accrdionHandlerX(idx);
-                    }
+                columns?.forEach((column, idx) => {
+                    column.addEventListener('click', () => {
+                        if (column.classList.contains('active')) {
+                            removeActive();
+                        } else {
+                            accrdionHandlerX(idx);
+                        }
+                    });
                 });
-            });
-        }
+            }
+        };
+        window.addEventListener('resize', () => {
+            accordionHandler();
+        });
+        accordionHandler();
     });
 };
 fourthSectionFunction();
--- src/components/SecondSection/index.ts
+++ src/components/SecondSection/index.ts
@@ -7,20 +7,22 @@ export const SecondSectionFunction = () => {
     const video = (document.querySelector('.video-bg .video')) as HTMLVideoElement;
     const playBtn = (document.querySelector('.playBtn')) as HTMLElement;
     const infoContainer = (document.querySelector('.info-container')) as HTMLElement;
+    const overlay = (document.querySelector('.overlay')) as HTMLElement;
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     let isPlaying = false;
 
-    video.addEventListener('click', () => {
+    const videoHandler = () => {
         isPlaying ? infoContainer.style.opacity = '1' : infoContainer.style.opacity = '0.8';
         !isPlaying ? playBtn.style.opacity = '0' : playBtn.style.opacity = '1';
         isPlaying ? video.pause() : video.play();
         isPlaying = !isPlaying;
+    };
+
+    overlay.addEventListener('click', () => {
+        videoHandler();
     });
     playBtn.addEventListener('click', () => {
-        isPlaying = true;
-        video.play();
-        isPlaying ? playBtn.style.opacity = '0' : playBtn.style.opacity = '1';
-        isPlaying ? infoContainer.style.opacity = '0.8' : infoContainer.style.opacity = '0.8';
+        videoHandler();
     });
 };
 
